c83a6957edef08dbb14602e37b09fb3eb39a5d9c
Axelle-Ga <axelle.gaige@gmail.com>
2022-02-09 17:41:52 +0100

    display a graph

--- Centrale/appCentraleVue/app-centrale-vue/src/components/graph.vue
+++ Centrale/appCentraleVue/app-centrale-vue/src/components/graph.vue
@@ -1,42 +1,66 @@
 <template>
-  <div>
-    <canvas id="chart"></canvas>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js'
+  import LineChart from './LineChart.js'
 
-export default {
-  name: 'graph'
-}
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        console.log("fillData");
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
 </script>
 
-
-<script>
-
-export default {
- components: {
- },
- data () {
-   return {
-   }
- },
- created(){
-    fetch('http://piensg028:8080/data/Temperature')
-    .then(result => {
-        return(result.json());
-    })
-    .then(data => {
-        //TODO attention l'api va changer il faudra modifier
-        console.log(data['temperature']['value'][0]);
-        let temperature = data['temperature']['value'][0];
-        console.log(temperature);
-    })
- },
- methods: {
- }
-}
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
 
 
-</script>
+
+    // fetch('http://piensg028:8080/data/Temperature')
+    // .then(result => {
+    //     return(result.json());
+    // })
+    // .then(data => {
+    //     //TODO attention l'api va changer il faudra modifier
+    //     console.log(data['temperature']['value'][0]);
+    //     let temperature = data['temperature']['value'][0];
+    //     console.log(temperature);
+    // })
