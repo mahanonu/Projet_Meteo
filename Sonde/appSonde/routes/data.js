ab15b0498054bfc3d9244acdb2693c44afd271b1
Axelle-Ga <axelle.gaige@gmail.com>
2022-02-09 14:22:54 +0100

    /measure return only last value

--- Sonde/appSonde/routes/data.js
+++ Sonde/appSonde/routes/data.js
@@ -39,7 +39,7 @@ router.get('/:measure', function(req, res, next) {
         valeurs[param] ={'date':[],'value':[]};
         allPromises.push(
         influx.query(
-            `select * from ${listMeasure[param]}`
+            `select * from ${listMeasure[param]} GROUP BY * ORDER BY DESC LIMIT 1`
         ))
     });
 
@@ -72,52 +72,6 @@ router.get('/:measure', function(req, res, next) {
     })
     
     });   
-
-    router.get('/:measure', function(req, res, next) {
-    let listParam = req.params['measure'].split(',').map(elem => elem.toLowerCase());
-    //Creata an influx client using default db (express_response_db) for now
-    //Creata an influx client using default db (express_response_db) for now
-    const influx = createInfluxClient();
-
-    let valeurs = {};
-    let allPromises = [];
-    listParam.forEach(param => {
-        valeurs[param] ={'date':[],'value':[]};
-        allPromises.push(
-        influx.query(
-            `select * from ${listMeasure[param]}`
-        ))
-    });
-
-    Promise.all(allPromises)
-    .then(promises => {
-        i=0
-        promises.forEach(results=>{
-            if (listParam[i]=='windvelocity'){
-                for (let index = 0; index < results.length; index++) {
-                    valeurs[listParam[i]]['value'].push({'min':results[index].min,'avg':results[index].moy, 'max':results[index].max});
-                    valeurs[listParam[i]]['date'].push(results[index].time._nanoISO);
-                }
-            }
-            else if (listParam[i]=='gpsposition'){
-                for (let index = 0; index < results.length; index++) {
-                    valeurs[listParam[i]]['value'].push({'east':results[index].est,'north':results[index].nord});
-                    valeurs[listParam[i]]['date'].push(results[index].time._nanoISO);
-                }
-            }
-            else{
-                for (let index = 0; index < results.length; index++) {
-                    valeurs[listParam[i]]['value'].push(results[index].value);
-                    valeurs[listParam[i]]['date'].push(results[index].time._nanoISO);
-                }
-            }
-            
-            i+=1;
-            })
-            res.send(valeurs);
-    })
-    
-    });
     
 
 function createInfluxClient(){
