1df225dcee7d514c6b60d5ec8a621c322869b2a3
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-05 12:51:53 +0100

    Add : map & marker components & leaflet npm

---
+++ Centrale/vue-black-dashboard-master/src/components/Perso/carte.vue
@@ -0,0 +1,72 @@
+<template>
+ <l-map
+   :center="center"
+   :zoom="zoom"
+   class="map"
+   ref="map"
+   @update:zoom="zoomUpdated"
+   @update:center="centerUpdated"
+ >
+   <l-tile-layer
+     :url="url"
+   >
+   </l-tile-layer>
+   <MarkerSonde
+    v-for="marker in markers"
+    :key="marker.id"
+    :marker="marker"
+    >
+    </MarkerSonde>
+   </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet';
+import MarkerSonde from './marker'
+import 'leaflet/dist/leaflet.css';
+
+export default {
+ components: {
+   LMap,
+   LTileLayer,
+   MarkerSonde
+ },
+ data () {
+   return {
+     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+     center: [ 49.1193089, 6.1757156 ],
+     zoom: 12,
+     markers:[]
+   }
+ },
+created(){
+    fetch('http://piensg031:8080/data/GPSPosition')
+    .then(result => {
+        return(result.json());
+    })
+    .then(data => {
+        //TODO attention l'api va changer il faudra modifier
+        console.log(data['gpsposition']['value'][0]['lon']);
+        let mark = {id: 1, imageUrl: require('@/assets/img/logo_raspberrypi.png'), coordinates: [ parseFloat(data['gpsposition']['value'][0]['lat']), parseFloat(data['gpsposition']['value'][0]['lon']) ]};
+        console.log(mark);
+        this.markers.push(mark);
+    })
+ },
+ methods: {
+   zoomUpdated (zoom) {
+     this.zoom = zoom;
+     console.log(this.markers)
+   },
+   centerUpdated (center) {
+     this.center = center;
+   }
+ }
+}
+</script>
+
+<style>
+ .map {
+   width: 100%;
+   height: 100%;
+ }
+</style>
