91dce6f67af6c4e0dfee6351d4b8f5a5310ebf0b
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-05 17:35:07 +0100

    Add display position of all raspberry

--- Centrale/vue-black-dashboard-master/src/components/Perso/carte.vue
+++ Centrale/vue-black-dashboard-master/src/components/Perso/carte.vue
@@ -1,72 +1,92 @@
 <template>
- <l-map
-   :center="center"
-   :zoom="zoom"
-   class="map"
-   ref="map"
-   @update:zoom="zoomUpdated"
-   @update:center="centerUpdated"
- >
-   <l-tile-layer
-     :url="url"
-   >
-   </l-tile-layer>
-   <MarkerSonde
-    v-for="marker in markers"
-    :key="marker.id"
-    :marker="marker"
-    >
+  <l-map
+    :center="center"
+    :zoom="zoom"
+    class="map"
+    ref="map"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+  >
+    <l-tile-layer :url="url"> </l-tile-layer>
+    <MarkerSonde v-for="marker in markers" :key="marker.id" :marker="marker">
     </MarkerSonde>
-   </l-map>
+  </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer } from 'vue2-leaflet';
-import MarkerSonde from './marker'
-import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer } from "vue2-leaflet";
+import MarkerSonde from "./marker";
+import "leaflet/dist/leaflet.css";
 
 export default {
- components: {
-   LMap,
-   LTileLayer,
-   MarkerSonde
- },
- data () {
-   return {
-     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-     center: [ 49.1193089, 6.1757156 ],
-     zoom: 12,
-     markers:[]
-   }
- },
-created(){
-    fetch('http://piensg031:8080/data/GPSPosition')
-    .then(result => {
-        return(result.json());
-    })
-    .then(data => {
+  components: {
+    LMap,
+    LTileLayer,
+    MarkerSonde,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      center: [49.1193089, 6.1757156],
+      zoom: 12,
+      markers: [],
+    };
+  },
+  created() {
+    let sonde = ["031", "028", "032"];
+    let urls = [];
+    for (let index = 0; index < sonde.length; index++) {
+      urls.push(`http://piensg${sonde[index]}:8080/data/GPSPosition`);
+    }
+
+    Promise.all(urls.map((u) => fetch(u)))
+      .then((responses) => Promise.all(responses.map((res) => res.json())))
+      .then((datas) => {
+        let id = 1;
+        datas.forEach(data => {
+          console.log(data);
         //TODO attention l'api va changer il faudra modifier
-        console.log(data['gpsposition']['value'][0]['lon']);
-        let mark = {id: 1, imageUrl: require('@/assets/img/logo_raspberrypi.png'), coordinates: [ parseFloat(data['gpsposition']['value'][0]['lat']), parseFloat(data['gpsposition']['value'][0]['lon']) ]};
-        console.log(mark);
+        console.log(data["gpsposition"]["value"][0]["lon"]);
+        let mark = {
+          id: id,
+          imageUrl: require("@/assets/img/logo_raspberrypi.png"),
+          coordinates: [
+            parseFloat(data["gpsposition"]["value"][0]["lat"]),
+            parseFloat(data["gpsposition"]["value"][0]["lon"]),
+          ],
+        };
         this.markers.push(mark);
-    })
- },
- methods: {
-   zoomUpdated (zoom) {
-     this.zoom = zoom;
-     console.log(this.markers)
-   },
-   centerUpdated (center) {
-     this.center = center;
-   }
- }
-}
+        id+=1;
+        });
+        
+      });
+
+    // fetch('http://piensg031:8080/data/GPSPosition')
+    // .then(result => {
+    //     return(result.json());
+    // })
+    // .then(data => {
+    //     //TODO attention l'api va changer il faudra modifier
+    //     console.log(data['gpsposition']['value'][0]['lon']);
+    //     let mark = {id: 1, imageUrl: require('@/assets/img/logo_raspberrypi.png'), coordinates: [ parseFloat(data['gpsposition']['value'][0]['lat']), parseFloat(data['gpsposition']['value'][0]['lon']) ]};
+    //     console.log(mark);
+    //     this.markers.push(mark);
+    // })
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+  },
+};
 </script>
 
 <style>
- .map {
-   width: 100%;
-   height: 100%;
- }
+.map {
+  width: 100%;
+  height: 100%;
+}
 </style>
