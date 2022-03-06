2105f6fd5a43a5ffc7e22acdb2f99e84a163b808
t.herman <hermantheo@hotmail.com>
2022-03-06 18:05:46 +0100

    v2 that almost works

--- Centrale/vue-black-dashboard-master/src/pages/Sonde.vue
+++ Centrale/vue-black-dashboard-master/src/pages/Sonde.vue
@@ -9,7 +9,7 @@
               <i slot="title" class="tim-icons icon-settings-gear-63"></i>
               <a class="dropdown-item" @click="changeSonde" href="#sonde">{{$t('Sonde028')}}</a>
               <a class="dropdown-item" @click="changeSonde" href="#sonde">{{$t('Sonde031')}}</a>
-              <!--<a class="dropdown-item" @click="changeSonde" href="#sonde">{{$t('Sonde032')}}</a>-->
+              <a class="dropdown-item" @click="changeSonde" href="#sonde">{{$t('Sonde032')}}</a>  
             </base-dropdown>
       <h2>Sonde {{ sonde }}</h2>
       <div class="col-12">
@@ -24,18 +24,64 @@
       </div>
     </div>
     <div class="row">
+      <base-dropdown menu-on-left=""
+                           tag="div"
+                           title-classes="btn btn-link btn-icon"
+                           aria-label="Settings menu"
+                           >
+              <i slot="title" class="tim-icons icon-settings-gear-63"></i>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('temperature')}}</a>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('pressure')}}</a>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('hygrometry')}}</a>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('brightness')}}</a>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('rainfall')}}</a>
+              <a class="dropdown-item" @click="changeChart" href="#sonde">{{$t('windvelocity')}}</a>
+            </base-dropdown>
+      <h2>{{ chart }}</h2>
       <div class="col-12">
         <card type="chart">
           <template slot="header">
-            <h5 class="card-category">{{$t('temperature')}}</h5>
+            <h5 class="card-category">{{ chart }}</h5>
+            
           </template>
           <div class="chart-area">
             <line-chart style="height: 100%"
                         chart-id="purple-line-chart"
-                        :chart-data="purpleLineChart.chartData"
-                        :gradient-colors="purpleLineChart.gradientColors"
-                        :gradient-stops="purpleLineChart.gradientStops"
-                        :extra-options="purpleLineChart.extraOptions">
+                        ref="meteoChar"
+                        :chart-data="meteoChar.chartData"
+                        :gradient-colors="meteoChar.gradientColors"
+                        :gradient-stops="meteoChar.gradientStops"
+                        :extra-options="meteoChar.extraOptions">
+            </line-chart>
+          </div>
+        </card>
+      </div>
+    </div>
+    <div class="row">
+      <base-dropdown menu-on-left=""
+                           tag="div"
+                           title-classes="btn btn-link btn-icon"
+                           aria-label="Settings menu"
+                           >
+              <i slot="title" class="tim-icons icon-settings-gear-63"></i>
+              <a class="dropdown-item" @click="changeWind" href="#sonde">{{$t('max wind velocity')}}</a>
+              <a class="dropdown-item" @click="changeWind" href="#sonde">{{$t('min wind velocity')}}</a>
+              <a class="dropdown-item" @click="changeWind" href="#sonde">{{$t('avg wind velocity')}}</a>
+            </base-dropdown>
+      <h2>{{ wind }}</h2>
+      <div class="col-12">
+        <card type="chart">
+          <template slot="header">
+            <h5 class="card-category">{{$t('wind velocity')}}</h5>
+          </template>
+          <div class="chart-area">
+            <line-chart style="height: 100%"
+                        chart-id="purple-line-chart"
+                        ref="windChar"
+                        :chart-data="windChar.chartData"
+                        :gradient-colors="windChar.gradientColors"
+                        :gradient-stops="windChar.gradientStops"
+                        :extra-options="windChar.extraOptions">
             </line-chart>
           </div>
         </card>
@@ -68,7 +114,33 @@ export default {
         data: null
       },
       sonde: "028",
-      purpleLineChart: {
+      chart: "temperature",
+      wind: "max wind velocity",
+      meteoChar: {
+          extraOptions: chartConfigs.purpleChartOptions,
+          chartData: {
+            labels: [],
+            datasets: [{
+              label: "Data",
+              fill: true,
+              borderColor: config.colors.primary,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.primary,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.primary,
+              pointBorderWidth: 2,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 0.4,
+              data: [],
+            }]
+          },
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.2, 0],
+        },
+      windChar: {
           extraOptions: chartConfigs.purpleChartOptions,
           chartData: {
             labels: [],
@@ -85,7 +157,7 @@ export default {
               pointBorderWidth: 20,
               pointHoverRadius: 4,
               pointHoverBorderWidth: 15,
-              pointRadius: 4,
+              pointRadius: 0.4,
               data: [],
             }]
           },
@@ -98,6 +170,7 @@ export default {
   mounted () {
       this.fillData()
       this.fillChart()
+      this.fillWind()
       this.$root.$on('bv::dropdown::show', bvEvent => {
         console.log('Dropdown is about to be shown', bvEvent)
       }) 
@@ -106,6 +179,16 @@ export default {
       changeSonde(event){
         this.sonde = event.originalTarget.innerHTML.split('Sonde')[1];
         this.fillData();
+        this.fillChart();
+        this.fillWind();
+      },
+      changeChart(event){
+        this.chart = event.originalTarget.innerHTML;
+        this.fillChart();
+      },
+      changeWind(event){
+        this.wind = event.originalTarget.innerHTML;
+        this.fillWind();
       },
       fillData () {
         fetch(`http://piensg${this.sonde}:8080/data/temperature,pressure,rainfall,hygrometry,brightness,winddirection,windvelocity,gpsposition`)
@@ -114,7 +197,7 @@ export default {
         })
         .then(data => {
             this.table1.data = [
-                {
+                { 
                     data: "temperature",
                     time: data['temperature']['date'][0],
                     value: data['temperature']['value'][0]
@@ -175,21 +258,27 @@ export default {
         
       },
       fillChart () {
-        fetch(`http://piensg${this.sonde}:8080/data/temperature/${new Date(Date.now()-86400000*7).toISOString()}`)
+        fetch(`http://piensg${this.sonde}:8080/data/${this.chart}/${new Date(Date.now()-86400000*7).toISOString()}`)
         .then(result => {
             return(result.json());
         })
         .then(data => { 
+
           let values = [];
           let dates = [];
-          
-          let i = 0;
-          data['temperature']['date'].forEach(element => {
-              dates.push(i++);
+          let i=0;
+          if (this.chart=='rainfall'){
+            data[this.chart]['value'].forEach(element => {
+              values.push(i++);
           });
-          data['temperature']['value'].forEach(element => {
-              values.push(i++)
+          }else{
+            data[this.chart]['value'].forEach(element => {
+              values.push(element);
           });
+          }
+          data[this.chart]['date'].forEach(element => {
+              dates.push(element);
+          });          
           let chartData = {
             labels: dates,
             datasets: [{
@@ -209,7 +298,50 @@ export default {
               data: values,
             }]
           };
-          this.purpleLineChart.updateGradients(chartData);
+          this.$refs.meteoChar.updateGradients(chartData);
+          this.meteoChar.chartData = chartData;
+        })
+      },
+      fillWind () {
+        fetch(`http://piensg${this.sonde}:8080/data/windvelocity/${new Date(Date.now()-86400000).toISOString()}`)
+        .then(result => {
+            return(result.json());
+        })
+        .then(data => { 
+
+          let values = [];
+          let dates = [];
+          let i=0;
+
+          data['windvelocity']['value'].forEach(element => {
+              values.push(element[this.wind.split(' ')[0]]);
+          });
+          
+          data['windvelocity']['date'].forEach(element => {
+              dates.push(element);
+          });          
+          console.log(data);
+          let chartData = {
+            labels: dates,
+            datasets: [{
+              label: "Data",
+              fill: true,
+              borderColor: config.colors.primary,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.primary,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.primary,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: values,
+            }]
+          };
+          this.$refs.windChar.updateGradients(chartData);
+          this.windChar.chartData = chartData;
         })
       } 
   }
