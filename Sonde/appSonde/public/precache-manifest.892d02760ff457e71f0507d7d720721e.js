6a47ea85a67c092a2b697667260692ba0644cd5b
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-07 15:43:13 +0100

    Add : add dist to deploy appCentrale with API

---
+++ Sonde/appSonde/public/precache-manifest.892d02760ff457e71f0507d7d720721e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/apple-icon.png"
+  },
+  {
+    "revision": "13fad59649938296ff34",
+    "url": "/css/app.fbb3dfde.css"
+  },
+  {
+    "revision": "f535a9dcd1eaa815ce85",
+    "url": "/css/common.20f63857.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/img/james.jpg"
+  },
+  {
+    "revision": "89b8155cd079e2baca373cc8a0934471",
+    "url": "/img/logo_raspberrypi.89b8155c.png"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/img/mike.jpg"
+  },
+  {
+    "revision": "735b8b9b5c39b1bf80a53240a3a188ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13fad59649938296ff34",
+    "url": "/js/app.25861dce.js"
+  },
+  {
+    "revision": "f39c1517b82dd8d6abe8",
+    "url": "/js/chunk-vendors.572c8397.js"
+  },
+  {
+    "revision": "f535a9dcd1eaa815ce85",
+    "url": "/js/common.f705aae4.js"
+  },
+  {
+    "revision": "903575ed823f8f8db5f7",
+    "url": "/js/common~dashboard.2ea54722.js"
+  },
+  {
+    "revision": "7b34eb2deda0e3cfaedb",
+    "url": "/js/dashboard.ae847e2c.js"
+  },
+  {
+    "revision": "1a167cfd82a1a7480ea601fccc8d7e18",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
